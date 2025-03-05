74a76eec7d17d862147c31c19eb7604f89f645ff
saad <qwejtrs@gmail.com>
2025-03-05 13:55:55 +0200

    fix

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,7 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('index7');
+    return view('recruitment.index');
 })->name('home');
 Route::get('/about', function () {
     return view('about');
